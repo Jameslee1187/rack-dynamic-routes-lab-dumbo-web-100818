8b8dc6bdb0fbf1141036ea906264801fd239040c
jameslee1187 <jameslee1187@gmail.com>
2018-10-27 22:18:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,13 @@
  def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    if req.path.match(/items/)
+ 
+      items_list = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
       
       
       
