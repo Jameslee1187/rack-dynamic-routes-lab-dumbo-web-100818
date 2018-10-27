a73036563278657a4dd68b3c7c9996a53f028b41
jameslee1187 <jameslee1187@gmail.com>
2018-10-27 22:18:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@
  def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
     if req.path.match(/items/)
- 
       items_list = req.path.split("/items/").last #turn /songs/Sorry into Sorry
       song = @@songs.find{|s| s.title == song_title}
  
